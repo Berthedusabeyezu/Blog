a11d57666f0d7c3c4d83c03623a6dfd22374c8d8
Berthedusabeyuzu <berthe1030@gmail.com>
2019-03-04 09:12:20 +0200

    first commit

---
+++ app/main/views.py
@@ -0,0 +1,108 @@
+from flask import render_template,request,redirect,url_for,abort
+from . import main
+from ..models import Post,User,Comment
+from .forms import PostForm,CommentForm
+from flask_login import login_required, current_user
+from .forms import PostForm,UpdateProfile
+from .. import db,photos
+from ..email import mail_message
+from ..request import get_quote
+
+
+
+
+
+@main.route('/')
+def index():
+
+
+    posts=Post.query.all()
+   
+    title = 'Home - Welcome to The best Blog Post Web'
+    quote = get_quote()
+
+    return render_template('index.html', title = title, quote = quote, posts = posts)
+    #  return render_template('index.html', title = title, quote=quote, posts = posts)
+@main.route('/posts/<int:id>')
+def posts(post_id):
+
+    '''
+    View post page function that returns the post details page and its data
+    '''
+    return render_template('post.html')
+
+@main.route('/post/new', methods = ['GET','POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    
+    if form.validate_on_submit():
+        title = form.title.data
+        post = form.post.data
+        new_post = Post(title = title,description = post, user_id = current_user.id)
+
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('main.index',post = post ))
+
+    return render_template('new_post.html',post_form=form)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None: 
+        abort(404)
+  
+    return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update/',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
+
+
+@main.route('/comment/new/<int:id>', methods = ['GET','POST'])
+@login_required
+def new_comment(id):
+    form = CommentForm()
+    
+
+    if form.validate_on_submit():
+        comment = form.comment.data
+        new_comment = Comment(comment = comment, user_id = current_user.id)
+
+        db.session.add(new_comment)
+        db.session.commit()
+        return redirect(url_for('main.index'))
+    
+
+    return render_template('new_comment.html',comment_form=form)
+
+
+    
---
+++ app/models.py
@@ -0,0 +1,78 @@
+from . import db
+from . import login_manager
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+
+ 
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    pass_secure = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_secure = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
+    comment = db.relationship('Comment', backref='user',lazy='dynamic')
+    post = db.relationship('Post' ,backref='user', lazy='dynamic')
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
+
+    def __repr__(self):
+        return f'User {self.username}'
+  
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    comment=db.relationship('Comment',backref='post', lazy='dynamic')
+    
+def save_post():
+  
+    db.session.add(self)
+    db.session.commit()
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer,primary_key = True)
+    comment = db.Column(db.String(255))
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer,db.ForeignKey('posts.id'))
+
+def save_comment():
+
+    db.session.add(new_comment)
+    db.session.commit()
+   
+       
+class Quote:
+    '''
+    Quote class to define Quote Objects
+    '''
+
+    def __init__(self,id,author,quote):
+        self.id =id
+        self.author = author
+        self.quote = quote
