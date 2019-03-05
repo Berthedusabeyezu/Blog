de2230a3b82981e96cb85010dc904dddaf67e5fc
Berthedusabeyuzu <berthe1030@gmail.com>
2019-03-05 18:30:04 +0200

    deployment to heroku

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
 
-    NEWS_API_BASE_URL='http://quotes.stormconsultancy.co.uk/random.json'
+    QOUTE_API_BASE_URL='http://quotes.stormconsultancy.co.uk/random.json'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:1234@localhost/blog'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
